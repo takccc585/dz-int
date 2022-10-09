288a7a7587d9b991d0edea1854132870ff39c620
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-10-09 12:39:14 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//int20
+	/*int a;
+	cin >> a;
+	float b = a / 3600;
+	cout << b;
+	return 0;*/
+	//int21
+	/*int a;
+	cin >> a;
+	float b = a / 60;
+	int c = a - (b * 60);
+	cout << c;*/
+	return 0;
+}
